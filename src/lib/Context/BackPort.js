e428303324a8018d81b7fce8e5b616964d157610
Lev <levdanneman@gmail.com>
2021-05-15 20:52:11 +0300

    update

--- src/lib/Context/BackPort.js
+++ src/lib/Context/BackPort.js
@@ -1,3 +1,4 @@
-const BACK_PORT = `https://restaurant-menus-backend.herokuapp.com/api`;
+const BACK_PORT = `http://localhost:5000/api`;
+// const BACK_PORT = `https://restaurant-menus-backend.herokuapp.com/api`;
 
 export default BACK_PORT;
